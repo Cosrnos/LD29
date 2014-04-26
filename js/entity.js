aa1ebd80fa59964fc9fde307ad64f9274136bdab
Lios <me@cosrnos.com>
2014-04-26 08:38:54 -0500

    Abstract-ifys entities and shared methods/properties.

---
+++ js/entity.js
@@ -0,0 +1,127 @@
+var Entity = function () {
+
+	var currentRoom = null;
+	var items = [];
+
+	this.Id = 0;
+	this.Name = "";
+	this.Species = "";
+	this.Level = 1;
+	this.Gold = 0;
+	this.Exp = 1; //Exp to be awarded to the attacker on kill
+
+	this.Health = 10;
+	this.HealthDelta = 0;
+	this.Mana = 0;
+	this.ManaDelta = 0;
+	this.BaseAttack = 1;
+	this.BaseDefense = 1;
+	this.BaseMagic = 1;
+	this.ActionPoints = 1;
+
+	this.Equipment = {
+		Weapon: "None",
+		Armor: "None",
+		Accessory: "None"
+	};
+
+	//Start AI
+
+	this.Think = function () {
+		//AI Logic goes here.
+		this.Idle();
+	};
+
+	this.Idle = function () {};
+
+	this.Attack = function (pTarget) {};
+
+	this.Run = function () {};
+
+	//End AI
+	this.TakeDamage = function (pDamage, pAttacker) {
+		//TODO: Fix Defense algorithm
+		var realHit = Math.floor(pDamage - this.BaseDefense);
+		this.HealthDelta += realHit;
+		if (this.HealthDelta >= this.Health) {
+			pAttacker.NotifyKill(this);
+			this.Kill();
+		}
+	};
+
+	this.NotifyKill = function (pAttacker) {
+
+	};
+
+	this.GiveItem = function (pItem, pQuantity) {
+		for (var i = 0; i < pQuantity; i++) {
+			items.push(pItem);
+		}
+	};
+
+	this.HasItem = function (pItemName, pQuantity) {
+		var totalCount = 0;
+		pQuantity = pQuantity || 1;
+
+		for (var i = 0; i < items.length; i++) {
+			if (items[i].Name === pItemName) {
+				totalCount++;
+
+				if (totalCount >= pQuantity) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	};
+
+	this.TakeItem = function (pItemName, pQuantity) {
+		var toRemove = 0;
+		pQuantity = pQuantity || 1;
+
+		for (var i = 0; i < items.length; i++) {
+			if (items[i].Name === pItemName) {
+				toRemove.push;
+				if (toRemove.length === pQuantity)
+					break;
+			}
+		}
+
+		if (toRemove.length !== pQuantity) {
+			return false;
+		}
+
+		for (var x = 0; x < toRemove.length; x++) {
+			items.splice(toRemove[x], 1);
+		}
+
+		return true;
+	};
+
+	this.UseItem = function (pItemName) {
+		var item = -1;
+		for (var i = 0; i < items.length; i++) {
+			if (items[i].Name === pItemName) {
+				item = items[i];
+				break;
+			}
+		}
+
+		if (item !== -1) {
+			item.Use(this);
+			Lynx.Log("Hero " + this.Name + " has used a(n) " + item.Name);
+			items.splice(item, 1);
+		}
+	};
+
+	this.SetRoom = function (pRoom) {
+		if (pRoom instanceof Room) {
+			currentRoom = pRoom;
+		}
+	};
+
+	this.Kill = function () {
+		Lynx.Log("Entity " + this.Name + " has been killed!");
+	};
+};
--- js/hero.js
+++ js/hero.js
@@ -1,9 +1,9 @@
 var Hero = function () {
 
-	var currentRoom = null;
-	var items = [];
 	var totalExp = 0;
 	var nextLevelExp = 100;
+
+	Entity.call(this);
 
 	Object.defineProperty(this, "Experience", {
 		get: function () {
@@ -20,39 +20,12 @@ var Hero = function () {
 		}
 	});
 
-	this.Id = 0;
-	this.Name = "";
+	this.Species = "Human";
 	this.Class = HeroClass.WARRIOR;
-	this.Level = 1;
-	this.Gold = 100;
-
-	this.Health = 20;
-	this.HealthDelta = 0;
-	this.Mana = 10;
-	this.ManaDelta = 0;
-	this.BaseAttack = 1;
-	this.BaseDefense = 1;
-	this.BaseMagic = 1;
-	this.ActionPoints = 3;
-
-	this.Equipment = {
-		Weapon: "Sword",
-		Armor: "Chain",
-		Accessory: "Severed human hand"
-	};
-
 	//Start AI
-
-	this.Think = function () {
-		//AI Logic goes here.
-		this.Idle();
+	this.NotifyKill = function (pEntityKilled) {
+		this.Experience += pEntityKilled.Exp;
 	};
-
-	this.Idle = function () {};
-
-	this.Attack = function (pTarget) {};
-
-	this.Run = function () {};
 
 	this.LevelUp = function () {
 		this.Health += 2;
@@ -60,84 +33,7 @@ var Hero = function () {
 		Lynx.Log("Hero " + this.Name + " Has leveled up! (" + this.Level + ")");
 	};
 
-	this.TakeDamage = function (pDamage, pAttacker) {
-		//TODO: Fix Defense algorithm
-		var realHit = Math.floor(pDamage - this.BaseDefense);
-		this.HealthDelta += realHit;
-		if (this.HealthDelta >= this.Health) {
-			this.Kill();
-		}
-	};
-
 	//End AI
-
-	this.GiveItem = function (pItem, pQuantity) {
-		for (var i = 0; i < pQuantity; i++) {
-			items.push(pItem);
-		}
-	};
-
-	this.HasItem = function (pItemName, pQuantity) {
-		var totalCount = 0;
-		pQuantity = pQuantity || 1;
-
-		for (var i = 0; i < items.length; i++) {
-			if (items[i].Name === pItemName) {
-				totalCount++;
-
-				if (totalCount >= pQuantity) {
-					return true;
-				}
-			}
-		}
-
-		return false;
-	};
-
-	this.TakeItem = function (pItemName, pQuantity) {
-		var toRemove = 0;
-		pQuantity = pQuantity || 1;
-
-		for (var i = 0; i < items.length; i++) {
-			if (items[i].Name === pItemName) {
-				toRemove.push;
-				if (toRemove.length === pQuantity)
-					break;
-			}
-		}
-
-		if (toRemove.length !== pQuantity) {
-			return false;
-		}
-
-		for (var x = 0; x < toRemove.length; x++) {
-			items.splice(toRemove[x], 1);
-		}
-
-		return true;
-	};
-
-	this.UseItem = function (pItemName) {
-		var item = -1;
-		for (var i = 0; i < items.length; i++) {
-			if (items[i].Name === pItemName) {
-				item = items[i];
-				break;
-			}
-		}
-
-		if (item !== -1) {
-			item.Use(this);
-			Lynx.Log("Hero " + this.Name + " has used a(n) " + item.Name);
-			items.splice(item, 1);
-		}
-	};
-
-	this.SetRoom = function (pRoom) {
-		if (pRoom instanceof Room) {
-			currentRoom = pRoom;
-		}
-	};
 
 	this.Kill = function () {
 		Lynx.Log("Hero " + this.Name + " has been killed!");
