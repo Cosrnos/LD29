d1ec7b0824456b49ed24a17630a2a86d67ed2ceb
Lios <me@cosrnos.com>
2014-04-27 10:20:10 -0500

    UI Fixes & Asset addition.

--- js/UI/Menu.js
+++ js/UI/Menu.js
@@ -12,13 +12,13 @@ var Menu = function (pName, pClose) {
 	var element = document.createElement("div");
 	element.id = "ui-" + pName;
 	element.style.position = "absolute";
-	element.style.zIndex = 1;
+	element.style.zIndex = 9;
 	element.style.position.top = 0;
 	element.style.position.left = 0;
-	element.style.background = "#efefef";
+	element.style.background = "#c7b299";
 	element.style.padding = "10px 10px";
 	element.style.width = "300px";
-	element.style.border = "3px solid #868686";
+	element.style.border = "3px solid #362f2d";
 	element.style.borderRadius = "5px";
 
 	element.style.visibility = "hidden";
@@ -28,24 +28,25 @@ var Menu = function (pName, pClose) {
 
 		that.Element = document.createElement("button");
 		that.Element.innerHTML = pName;
-		that.Element.style.background = "rgba(0,175, 55, 0.4)";
+		that.Element.style.background = "#362f2d";
 		that.Element.style.border = "1px solid #000000";
 		that.Element.style.borderRadius = "5px";
 
 		that.Element.style.width = "100%";
 		that.Element.style.display = "block";
-		that.Element.style.color = "#333333";
+		that.Element.style.color = "#ffffff";
+		that.Element.style.fontSize = "18px";
 		that.Element.style.outline = "0px";
 		that.Element.style.cursor = "pointer";
-		
-		that.Element.onmouseover = function(){
-			that.Element.style.background = "rgba(0, 175, 55, 0.2)";
+
+		that.Element.onmouseover = function () {
+			that.Element.style.background = "#534741";
 		};
-		
-		that.Element.onmouseout = function(){
-			that.Element.style.background = "rgba(0, 175, 55, 0.4)";
+
+		that.Element.onmouseout = function () {
+			that.Element.style.background = "#362f2d";
 		};
-		
+
 		that.Element.onclick = function (e) {
 			if (pClickCallback.call(pParent.Target))
 				pParent.Hide();
@@ -67,6 +68,17 @@ var Menu = function (pName, pClose) {
 			options.splice(options.indexOf(pO), 1);
 		}
 	};
+
+	this.MoveBy = function (pX, pY) {
+		if (pX === 0 && pY === 0)
+			return;
+
+		this.X -= pX;
+		this.Y -= pY;
+
+		element.style.top = this.Y + "px";
+		element.style.left = this.X + "px";
+	}
 
 	this.ShowAt = function (pX, pY) {
 		this.X = pX;
@@ -90,8 +102,8 @@ var Menu = function (pName, pClose) {
 				return true;
 			}, this).Element);
 		}
-		element.style.left = pX + "px";
-		element.style.top = pY + "px";
+		element.style.left = this.X + "px";
+		element.style.top = this.Y + "px";
 		element.style.visibility = "visible";
 		this.Disposed = false;
 		Game.ActiveMenu = this;
